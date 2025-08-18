88256730ea7865c7b192235501309d374065b650
Aleksei Rozanov <rozan012@acn59.agate.msi.umn.edu>
2025-08-18 09:12:31 -0500

    figures

--- pipeline/shedule_day.sh
+++ pipeline/shedule_day.sh
@@ -7,12 +7,12 @@
 #SBATCH --partition=msismall#msigpu
 #SBATCH --mem=100G
 #SBATCH --cpus-per-task=2
-#SBATCH --array=1-12 #1-2211%50          # Skip first 30, process files 31-2211
+#SBATCH --array=1-2 #%50          # Skip first 30, process files 31-2211
 
 source ~/.bashrc
 conda activate ET_lightgbm
 
-#files=($(find /home/runck014/shared/et_upscaling/MODIS/2024 -name "MODIS_2024_*.tif" | sort))
+#files=($(find /home/runck014/shared/et_upscaling/MODIS/2019 -name "MODIS_2019_*.tif" | sort))
 mapfile -t files < remaining_files.txt
 
 # Get the file for this array task (SLURM_ARRAY_TASK_ID is already 31-2211)
